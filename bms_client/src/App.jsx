b97ad59d0864be6cedd3b1c0363e242604daa517
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-03-27 10:40:56 +0600

    used trycatch block for error handling

--- bms_client/src/App.jsx
+++ bms_client/src/App.jsx
@@ -1,50 +1,57 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import abi from "./contractJSON/Booklist.json";
 import { ethers } from "ethers";
 
 function App() {
   const [account, setAccount] = useState("Not connected");
-
-  useEffect(() => {
-    const template = async () => {
-      //error handling with the users without metamask
-      if (window.ethereum === undefined) {
-        alert("Metmask is not installed. Please install it to continue");
-        return;
-      }
-
-      const contractAddress = "0xf31d48A55a18756D18848602c5d96Af8d0563027";
-      const contractABI = abi.abi;
-
-      const { ethereum } = window;
-      const account = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      setAccount(account);
-
-      const provider = new ethers.providers.Web3Provider(ethereum);
-
-      const signer = provider.getSigner();
-
-      const contract = new ethers.Contract(
-        contractAddress,
-        contractABI,
-        signer
-      );
-      console.log(contract);
-      setState({ provider, signer, contract });
-    };
-    template();
-  }, []);
-
   const [state, setState] = useState({
     provider: null,
     signer: null,
     contract: null,
   });
 
-  return <div></div>;
+  useEffect(() => {
+    const template = async () => {
+      if (window.ethereum === undefined) {
+        alert("MetaMask is not installed. Please install it to continue");
+        return;
+      }
+
+      try {
+        const contractAddress = "0xf31d48A55a18756D18848602c5d96Af8d0563027";
+        const contractABI = abi.abi;
+
+        const { ethereum } = window;
+        const accounts = await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setAccount(accounts[0]);
+
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        console.log(contract);
+        setState({ provider, signer, contract });
+      } catch (error) {
+        console.error("An error occurred:", error);
+        alert("An error occurred, please try again.");
+      }
+    };
+    template();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Account: {account}</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
